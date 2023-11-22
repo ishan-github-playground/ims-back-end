18339e2ff935badb6d8393f6cd8234444d103fe4
Ishanka Dilshan <ishnakadilshan64@gmail.com>
2023-11-22 16:31:50 +0530

    create_database

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,8 +3,18 @@ CREATE TABLE IF NOT EXISTS teacher(
     name VARCHAR(100) NOT NULL,
     contact VARCHAR(100) NOT NULL
 );
-CREATE TABLE IF NOT EXISTS teacher(
+CREATE TABLE IF NOT EXISTS course(
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     duration_in_months VARCHAR(100) NOT NULL
 );
+CREATE TABLE IF NOT EXISTS teacher_course {
+    teacher_id INT NOT NULL,
+    course_id INT NOT NULL,
+    CONSTRAINT PRIMARY KEY (teacher_id, course_id),
+    CONSTRAINT fk_1 FOREIGN KEY (teacher_id) REFERENCES teacher(id),
+    CONSTRAINT fk_2 FOREIGN KEY (course_id) REFERENCES course(id) ON DELETE CASCADE
+
+
+
+    }
